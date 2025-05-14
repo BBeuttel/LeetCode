18f38c44c2510e5f075f7ad985f13ab3bb6f7e52
Brandon <bbeuttel@gmail.com>
2025-05-14 15:55:48 -0700

    tested and 49

--- 20ValidParenthesis.js
+++ 20ValidParenthesis.js
@@ -20,3 +20,26 @@ var isValid = function(s) {
     }
     return stack.length === 0;
 };
+
+// check if string length modulo 2 is equal to zero
+// create object with closed parens as key and open as value.  iterate through string check value exists then push  it to the empty array
+// else check if key exists if  the popped value is not equal to the value of the iterated character return false
+//return true
+const isValid = (s) => {
+    const stack = [];
+    const pairs = {
+        ']': '[',
+        '}': '{',
+        ')': '('
+    }
+    for (char of s) {
+        if (Object.values(pairs).includes(char)){
+            stack.push(char);
+        } else if( Object.keys(pairs). includes(char)){
+            if(stack.pop() !== pairs[char]){
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+}
--- 66PlusOne.js
+++ 66PlusOne.js
@@ -16,3 +16,8 @@ var plusOne = function(digits) {
     digits.unshift(1);
     return digits;
 };
+
+//got to length of the array -1 and add 1 to the number
+//edge cases are for 9 if 9
+//iterate throughn array if value is less than 9 then add one ot the lenght of the array -1 return the array
+// if value is equal to 9 set value equal to zero
