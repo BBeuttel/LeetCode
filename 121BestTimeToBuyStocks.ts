b4e35272651cf274af14415da6d73f3355407f17
Brandon <bbeuttel@gmail.com>
2025-05-15 16:29:33 -0700

    121 optimized solution added

--- 121BestTimeToBuyStocks.ts
+++ 121BestTimeToBuyStocks.ts
@@ -16,3 +16,19 @@ function maxProfit(prices: number[]): number {
     }
     return maxProfit;
 };
+
+//Terrible Solution
+
+//Optimized Solution
+
+function maxProfitOptimized(prices: number[]): number {
+    let profit = 0;
+    let minPrice = prices[0];
+
+    for (let i = 1; i < prices.length; i++){
+        minPrice = Math.min(minPrice, prices[i]);
+        profit = Math.max(profit, prices[i] - minPrice);
+
+    }
+    return profit;
+}
