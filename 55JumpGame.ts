b43716cb06934bee268aa6516fcf730b91493856
Brandon <bbeuttel@gmail.com>
2025-05-28 10:02:35 -0700

    added psuedocode to 55

--- 55JumpGame.ts
+++ 55JumpGame.ts
@@ -2,15 +2,15 @@ function canJump(nums: number[]): boolean {
     if (nums.length === 1){
         return true
     }
-    let i = 0;
-    let count = nums[i];
+    let i: number = 0;
+    let count: number = nums[i];
     while(count > 0){
         count--;
         //testing if weve reached the end of the array
         if(i === nums.length - 1){
             return true;
         }
-        //checking that there is a value that is greater than
+        //checking that there is a value that is greater than the count and reassign the value of count if it is
         if(nums[i] > count){
             count = nums[i]
         }
