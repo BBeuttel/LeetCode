63354788a84ca7f66c6ab2b15801b57dbf31f2d9
Brandon <bbeuttel@gmail.com>
2025-05-28 09:58:12 -0700

    55 jump game complete

---
+++ 55JumpGame.ts
@@ -0,0 +1,20 @@
+function canJump(nums: number[]): boolean {
+    if (nums.length === 1){
+        return true
+    }
+    let i = 0;
+    let count = nums[i];
+    while(count > 0){
+        count--;
+        //testing if weve reached the end of the array
+        if(i === nums.length - 1){
+            return true;
+        }
+        //checking that there is a value that is greater than
+        if(nums[i] > count){
+            count = nums[i]
+        }
+        i++;
+    }
+    return false
+};
