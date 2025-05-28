968a297d601f5c7c64acf4f272ba785cf9688503
Brandon <bbeuttel@gmail.com>
2025-05-28 11:26:06 -0700

    psudo coded 134

--- 134GasStation.ts
+++ 134GasStation.ts
@@ -1,12 +1,15 @@
+//define total gas tank, the current Tank size, and the staritng index
+
 function canCompleteCircuit(gas: number[], cost: number[]): number {
     let totalTank: number = 0;
     let currTank: number = 0;
     let startIndex: number = 0;
-
+//iterate through gas array add gas - cost at i to total tank
+//current valeu of the tank will equal the same thing
     for (let i = 0; i < gas.length; i++) {
         totalTank += gas[i] - cost[i];
         currTank += gas[i] - cost[i];
-
+//check if the amount of the current tank is less than zero if it is the starting index is the next value in the gass array and you would restart the current tank value.
         if (currTank < 0) {
             startIndex = i + 1;
             currTank = 0;
